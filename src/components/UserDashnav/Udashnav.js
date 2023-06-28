0d3b82234f1947b621123914f73748163b6ffd3e
UchitChavda <uchit.chavda@somaiya.edu>
2023-06-28 17:54:40 +0530

    Updated User Pages

---
+++ src/components/UserDashnav/Udashnav.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Udashnav.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function UDashnav({ activeLink, handleLinkClick }) {
+  const navigate = useNavigate();
+      function userLogout(){
+        localStorage.removeItem('token');
+        navigate('/');
+      }
+  return (
+    <div className="das23">
+      <div className="prof">
+        <div className="social" id="social1"></div>
+        <div className="nam2">
+          <span className="name">Raj Batliwala</span>
+          <span className="gname">rajbatliwala23@gmail.com</span>
+        </div>
+      </div>
+      <div className="lis2">
+        <ul className="sidebar-nav">
+          <li>
+            <Link
+              // to="/profile"
+              className={activeLink === "profile" ? "bhola active" : "bhola"}
+              onClick={() => handleLinkClick("profile")}
+            >
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link
+              // to="/profile"
+              className={activeLink === "addwork" ? "bhola active" : "bhola"}
+              onClick={() => handleLinkClick("addwork")}
+            >
+              Add Work
+            </Link>
+          </li>
+          <li>
+            <Link
+              // to="/profile"
+              className={activeLink === "previous" ? "bhola active" : "bhola"}
+              onClick={() => handleLinkClick("previous")}
+            >
+              Previous Activity
+            </Link>
+          </li>
+          <li>
+            <Link
+              // to="/settings"
+              className={activeLink === "settings" ? "bhola active" : "bhola"}
+              onClick={() => handleLinkClick("settings")}
+            >
+              Settings
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="logout3">
+        <button onClick={userLogout} className="bhola1" href="#">
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default UDashnav;
--- src/page/user/Userdashboard.js
+++ src/page/user/Userdashboard.js
@@ -1,13 +1,46 @@
-import React from 'react'
-import './user.css'
-
+import AddWork from "../AddWork";
+import Previous from "../Previous";
+import Profile from "../Profile";
+// import Settings from "./Settings";
+import UDashnav from "../../components/UserDashnav/Udashnav";
+import { useState } from "react";
+import "./user.css";
+// ...
 
 function Userdashboard() {
+  // ...
+  const [activeLink, setActiveLink] = useState("profile");
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
+
+  // ...
+
+  // Render the content based on the activeLink state
+  const renderContent = () => {
+    switch (activeLink) {
+      case "profile":
+        return <Profile />;
+      case "addwork":
+        return <AddWork />;
+      case "previous":
+         return <Previous />;
+      
+      default:
+        return null;
+    }
+  };
+
   return (
-    <div>
-      welcome user
+    <div className="dsdf3">
+      <div className="ds1">
+        <UDashnav activeLink={activeLink} handleLinkClick={handleLinkClick} />
+      </div>
+      <div className="df2">
+        <div className="content34">{renderContent()}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Userdashboard
+export default Userdashboard;
