e9df90738b0ce9736d6c20c0e489faba4804ba51
kartikeym27 <kartikey.mishra@somaiya.edu>
2023-06-29 10:35:38 +0530

    use npm run both for running both server from base directory

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,8 @@
 export default{
     JWT_SECRET : "3usJi3F7VGLMLxjcfZcHpaBtRJAm3Y9ERLqNB+LSviI=",
 
-    EMAIL: "subhadeepsamanta26@gmail.com",
-    PASSWORD: "ragymgydmoobwlby",
-    ATLAS_URI : "mongodb+srv://subhadips:0dVHB07bJVuZltWl@cluster0.eijvoo5.mongodb.net/"
+    EMAIL: "mernship@gmail.com",
+    PASSWORD: "qkazxjigtsmlpjkz",
+    ATLAS_URI : "mongodb+srv://mernship:mernship7&R@mernship-1.n4vontn.mongodb.net/?retryWrites=true&w=majority"
 
 }
