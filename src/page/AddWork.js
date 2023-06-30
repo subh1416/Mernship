3169f7548435aca4c93790f286ac07ec24b52979
subh1416 <subhadipsamanta1416@gmail.com>
2023-06-30 12:51:51 +0530

    add work section done

--- src/page/AddWork.js
+++ src/page/AddWork.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function AddWork() {
+  const divStyle = {
+    height: "100px",
+  };
+
+  const colorjuw = {
+    backgroundColor: '#9b59b6',
+  };
   return (
     <div>
-        <h1>Add Work</h1>
+      <h1>Add Work</h1>
+
+      <div className="input6757">
+        <div className="form-floating my-4"  >
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea"
+            
+           
+          ></textarea>
+          <label for="floatingTextarea">Add Title</label>
+        </div>
+
+        <div className="form-floating">
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="floatingTextarea2"
+            style={divStyle}
+          ></textarea>
+          <label for="floatingTextarea2">Add Description</label>
+        </div>
+
+        <button type="button" class="btn btn-primary my-3" style={colorjuw}>
+          Upload File
+        </button>
+        <button type="button" class="btn btn-secondary my-3">
+          Save
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddWork
+export default AddWork;
