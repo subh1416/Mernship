123a10cba7701efe3fbee50a338e67dedf0d3503
subh1416 <subhadipsamanta1416@gmail.com>
2023-07-03 11:31:04 +0530

    degraded cards styled

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,48 +1,47 @@
 import React from 'react';
-import './cards.css'
+import './cards.css';
 
 function Cards() {
   return (
     <div>
       <div className="container">
-   <div className="row">
-     
-      <div className="card card-circle">
-         <div className="card-icon">
-            <i className="fas fa-apple-alt"></i>
-         </div>
-         <div className="card-body">
-            <h5 className="card-title">Apple</h5>
-            <p className="card-text">Some quick example text to build on the card title and card content.</p>
-            <button  className="btn btn-primary">Eat me</button>
-         </div>
+        <div className="row">
+          <div className="custom-card custom-card-circle">
+            <div className="custom-card-icon">
+              <i className="fas fa-apple-alt"></i>
+            </div>
+            <div className="custom-card-body">
+              <h5 className="custom-card-title">Apple</h5>
+              <p className="custom-card-text">Some quick example text to build on the card title and card content.</p>
+              <button className="custom-btn custom-btn-primary">Eat me</button>
+            </div>
+          </div>
+
+          <div className="custom-card custom-card-circle">
+            <div className="custom-card-icon">
+              <i className="fas fa-cookie"></i>
+            </div>
+            <div className="custom-card-body">
+              <h5 className="custom-card-title">Cookie</h5>
+              <p className="custom-card-text">Some quick example text to build on the card title and card content.</p>
+              <button className="custom-btn custom-btn-primary">Eat me</button>
+            </div>
+          </div>
+
+          <div className="custom-card custom-card-circle">
+            <div className="custom-card-icon">
+              <i className="fas fa-carrot"></i>
+            </div>
+            <div className="custom-card-body">
+              <h5 className="custom-card-title">Carrot</h5>
+              <p className="custom-card-text">Some quick example text to build on the card title and card content.</p>
+              <button className="custom-btn custom-btn-primary">Eat me</button>
+            </div>
+          </div>
+        </div>
       </div>
-      
-      <div className="card card-circle">
-         <div className="card-icon">
-            <i className="fas fa-cookie"></i>
-         </div>
-         <div className="card-body">
-            <h5 className="card-title">Cookie</h5>
-            <p className="card-text">Some quick example text to build on the card title and card content.</p>
-            <button  className="btn btn-primary">Eat me</button>
-         </div>
-      </div>
-      
-      <div className="card card-circle ">
-         <div className="card-icon">
-            <i className="fas fa-carrot"></i>
-         </div>
-         <div className="card-body">
-            <h5 className="card-title">Carrot</h5>
-            <p className="card-text">Some quick example text to build on the card title and card content.</p>
-            <button  className="btn btn-primary">Eat me</button>
-         </div>
-      </div>
-   </div>
-</div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
